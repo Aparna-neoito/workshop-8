6072605c67a80f595fd2d9219099115c3fcc6266
APARNAB <aparna.b@neoito.com>
2022-11-16 17:38:05 +0530

    workshop8 navbar hover fixed

--- src/script.js
+++ src/script.js
@@ -7,11 +7,11 @@ let toggle=document.querySelector('.toggle');
 toggle.addEventListener('click',function(){
   let activetoggle=this.nextElementSibling;
   activetoggle.style.display="flex";
-  activetoggle.style.height="800px";
+  activetoggle.style.height="900px";
   let close=this.nextElementSibling.nextElementSibling
   close.style.display="flex";
   let closetoggle=this.nextElementSibling.nextElementSibling;
-  closetoggle.addEventListener('click',function(){
+    closetoggle.addEventListener('click',function(){
     activetoggle.style.display="none";
     close.style.display="none";
   })
@@ -65,15 +65,32 @@ for(let i=0;i<accordion.length;i++){
 
 let email1btn1=document.getElementsByClassName('emailbtn1');
 let email1=document.getElementsByClassName('email1');
+let emaildiv=document.getElementsByClassName('emaildiv');
+let emailmsg=document.getElementsByClassName('emailmsg');
 
 for(let i=0;i<email1btn1.length;i++){
   email1btn1[i].addEventListener('click',function(){
   let text=email1[i].value;
+  email1[i].addEventListener('click',function(){
+    email1[i].value="";
+    email1[i].style.color="black";
+    emaildiv[i].style.borderColor="#107EC3";
+    emailmsg[i].textContent="";
+  })
   if (text=== "") {
-    alert("Email is required") ;
+  
+    emaildiv[i].style.borderColor="red";
+    emailmsg[i].textContent="*email is required";
+    emailmsg[i].style.fontSize="14px";
+    emailmsg[i].style.color="red";
+    console.log(emailmsg[i].textContent)
   }
   else if (!text.match(/^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/)) {
-      alert("Email must be a valid email") ;
+
+    emaildiv[i].style.borderColor="red";
+    emailmsg[i].style.borderColor="red";
+    emailmsg[i].textContent="*email must be a valid email";
+    emailmsg[i].style.color="red";
     }
 })
 
